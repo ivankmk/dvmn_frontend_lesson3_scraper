07fe62db56e3aab2585bb216766b2add7fde4711
IvanKMK <kumeyko@hotmail.com>
2020-08-14 23:16:52 +0100

    download books from tululu.org.

--- script.py
+++ script.py
@@ -1,9 +1,21 @@
 import requests
+import os
+import time
 
-url = 'http://tululu.org/txt.php?id=32168'
 
-response = requests.get(url)
-data = response.text
+def save_books(limit):
+    url_template = 'http://tululu.org/txt.php?id='
+    os.makedirs('books', exist_ok=True)
 
-with open('max_sands.txt', 'w') as f:
-    f.write(data)
+    for id in range(1, limit):
+        time.sleep(3)
+        response = requests.get(f'{url_template}{id}', allow_redirects=False)
+        if response.status_code == 200:
+            data = response.text
+
+            with open(f'books/book_id_{id}.txt', 'w') as f:
+                f.write(data)
+
+
+if __name__ == "__main__":
+    save_books(11)
