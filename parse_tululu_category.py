5a2f9192164ebf33f0eb11e3906f4de6a0d1199c
IvanKMK <kumeyko@hotmail.com>
2020-08-20 22:04:54 +0100

    download books from all pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,10 +5,12 @@ from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 
+
 def get_genres_collection_soup(id, page):
-    response = requests.get(f'http://tululu.org/l{id}/{page}', allow_redirects=False)
+    response = requests.get(
+        f'http://tululu.org/l{id}/{page}', allow_redirects=False)
     if response.status_code == 200:
-        soup =  BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, 'lxml')
         return soup
 
 
@@ -17,19 +19,18 @@ def get_books_url(soup):
     all_books_with_one_genre = soup.findAll('table', {'class': 'd_book'})
     books_url = [
         urljoin(base, book.find('a')['href']) for book in all_books_with_one_genre
-        ]
+    ]
     return books_url
+
 
 def collect_book_urls(pages_to_search, genre_id):
     url_collection = []
     for page_list in range(1, pages_to_search+1):
         soup = get_genres_collection_soup(genre_id, page_list)
         list_of_books = get_books_url(soup)
-        url_collection = url_collection.extend(list_of_books)
+        url_collection.extend(list_of_books)
     return url_collection
 
 
-
 if __name__ == "__main__":
-
-    print(collect_book_urls(3, 55))
+    pass
--- script.py
+++ script.py
@@ -4,15 +4,20 @@ import time
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
+from parse_tululu_category import collect_book_urls
+import json
 
-def get_book_soup(id):
-    response = requests.get(f'http://tululu.org/b{id}/', allow_redirects=False)
+
+def get_book_soup(book_url):
+    response = requests.get(book_url, allow_redirects=False)
     if response.status_code == 200:
-        soup =  BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, 'lxml')
         return soup
+
 
 def clean_filename(id, filename):
     return f'{id}-{sanitize_filename(filename)}'
+
 
 def get_title_and_author(soup):
     title, author = soup.find('h1').text.split('::')
@@ -22,19 +27,22 @@ def get_title_and_author(soup):
         'author': author.strip()
     }
 
+
 def get_cover_fullpath(soup):
     base = 'http://tululu.org'
     img_name = soup.find('div', {'class': 'bookimage'}).find('img')['src']
-    
+
     return urljoin(base, img_name)
+
 
 def get_comments(soup):
     all_comments = soup.findAll('div', {'class': 'texts'})
-    comments =  [
+    comments = [
         comment.find('span', {'class': 'black'}).text for comment in all_comments
-        ]
-    
+    ]
+
     return None if len(comments) == 0 else comments
+
 
 def get_genres(soup):
     genres_html = soup.find('span', {'class': 'd_book'})
@@ -54,16 +62,17 @@ def download_txt(book_id, filename, folder=None):
     filename_cleaned = clean_filename(book_id, filename)
     folder_to_save = folder or 'books'
     file_url = f'http://tululu.org/txt.php?id={book_id}'
-    
+
     os.makedirs(folder_to_save, exist_ok=True)
     response = requests.get(file_url, allow_redirects=False)
     full_path = os.path.join(folder_to_save, filename_cleaned)
-    
+
     if response.status_code == 200:
         data = response.text
-        with open(f'{full_path}.txt', 'w') as f:
+        full_path_with_ext = f'{full_path}.txt'
+        with open(full_path_with_ext, 'w') as f:
             f.write(data)
-        return full_path
+        return full_path_with_ext
 
 
 def download_image(image_url, folder=None):
@@ -78,14 +87,34 @@ def download_image(image_url, folder=None):
             f.write(data)
         return full_path
 
+
 if __name__ == "__main__":
-    for book_id in range(1, 11):
+    books_collection = []
+    book_urls = collect_book_urls(4, 55)
+    for book_url in book_urls:
         time.sleep(3)
-        book_soup = get_book_soup(book_id)
+        book_soup = get_book_soup(book_url)
         if book_soup:
-            print(get_genres(book_soup))
-            # title_and_author = get_title_and_author(book_soup)
-            # if download_txt(book_id, title_and_author['title'], folder=None):
-            #     cover_img = get_cover_fullpath(book_soup)
-            #     if cover_img:
-            #         download_image(cover_img, folder=None)
+            comments = get_comments(book_soup)
+            genres = get_genres(book_soup)
+            title_and_author = get_title_and_author(book_soup)
+            book_id = book_url.split('/')[-2].replace('b', '')
+            book_path = download_txt(
+                book_id, title_and_author['title'], folder=None)
+            if book_path:
+                img_src = get_cover_fullpath(book_soup)
+                if img_src:
+                    cover_link = download_image(img_src, folder=None)
+                    print('This book was saved: ', title_and_author['title'])
+                    books_collection.append(
+                        {
+                            'title': title_and_author['title'],
+                            'author': title_and_author['author'],
+                            'img_src': cover_link,
+                            'book_path': book_path,
+                            'comments': comments,
+                            'genres': genres
+                        }
+                    )
+    with open('books_db.json', 'w', encoding='utf8') as books_json_db:
+        json.dump(books_collection, books_json_db, ensure_ascii=False)
